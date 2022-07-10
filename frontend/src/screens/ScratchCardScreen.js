8509ab455e9b6d916df1aa56dbd8608244b1f2ca
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2022-07-10 16:25:24 +0530

    scratch card working

--- frontend/src/screens/ScratchCardScreen.js
+++ frontend/src/screens/ScratchCardScreen.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import { useMeasure } from 'react-use'
 import {
   Link,
   useParams,
@@ -13,7 +14,15 @@ import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone' // dependent on utc plugin
 
-import { Form, Image, Button, Container, Row, Card } from 'react-bootstrap'
+import {
+  Offcanvas,
+  Form,
+  Image,
+  Button,
+  Container,
+  Row,
+  Card,
+} from 'react-bootstrap'
 import { BreadcrumbsItem } from 'react-breadcrumbs-dynamic'
 import Breadcrumb from '../wrappers/Breadcrumb'
 
@@ -58,6 +67,9 @@ function ScratchCardScreen() {
   const [searchParams, setSearchParams] = useSearchParams()
   let location = useLocation()
   const navigate = useNavigate()
+
+  const [offCanvasRef, { offCanvasWidth, offCanvasHeight }] = useMeasure()
+  const [ref, { x, y, width, height, top, right, bottom, left }] = useMeasure()
 
   const [showCard, setShowCard] = useState(false)
   const [mobile, setMobile] = useState('')
@@ -114,124 +126,138 @@ function ScratchCardScreen() {
         Scratch Card
       </BreadcrumbsItem>
       <Breadcrumb />
-      <Container className='d-flex justify-content-center align-items-center mt-3'>
+
+      <Offcanvas
+        className='staff-ScratchCard'
+        show={showCard}
+        onHide={handleCardClose}
+        scroll={false}
+        placement='end'
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title className='mx-auto'></Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body ref={ref}>
+          <div>
+            {cardDetails && cardDetails.status === 'success' ? (
+              <ScratchCard
+                width={width}
+                height={height}
+                image={backgroundImageSrc}
+                finishPercent={30}
+                fadeOutOnComplete
+                // onComplete={!isCompleted && !cardScratchCompleted && addDigiDollasHandler}
+                onComplete={(e) => addDigiDollasHandler(e)}
+              >
+                <div
+                  className='scratch-card-bg'
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    // alignItems: 'center',
+                    // justifyContent: 'center',
+                    // background: '#E8E8E8',
+                  }}
+                >
+                  <>
+                    <Row className='no-gutters heading'>Congrats!</Row>
+                    {/* <Row className='no-gutters digiDollas'>{`${cardDetails.digiDollas}`}</Row> */}
+                    <Row className='no-gutters coinDigiDollas'>
+                      <Image
+                        className='scratch-coin'
+                        fluid
+                        src={cardDetails.product.image}
+                        alt={''}
+                        rounded
+                      ></Image>
+                    </Row>
+                    <Row className='no-gutters message'>{`Yay! You've won ${cardDetails.digiDollas}`}</Row>
+                    <Row className='note'>
+                      <p>{`This will be credited to your DigiDollas`}</p>
+                    </Row>
+                  </>
+                </div>
+              </ScratchCard>
+            ) : cardDetails && cardDetails.status === 'fail' ? (
+              <>
+                <div
+                  className='ScratchCard__Container'
+                  // style={{ width: '300px', height: '330px' }}
+                >
+                  <img
+                    className='img-fluid mx-auto'
+                    src={backgroundImageAlreadyScratched}
+                    alt=''
+                  ></img>
+                </div>
+                <h2 className='my-3'>
+                  Sorry! <br /> Come back later
+                </h2>
+                <h2>
+                  <Countdown
+                    className='digicel-gradient-text'
+                    date={dayjs() + dayjs().endOf('day').diff(dayjs())}
+                  />
+                </h2>
+              </>
+            ) : (
+              <>
+                <h1>Please login to scratch the card</h1>
+                <Link to='/login' className='btn btn-primary digicel-button'>
+                  Login
+                </Link>
+              </>
+            )}
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+
+      <Container className='d-flex justify-content-center align-items-center'>
         <Card style={{ width: '25rem' }}>
           <Card.Body>
             <h1 className='scratch-card-title text-center'>SCRATCH N’ WIN</h1>
-            <div>
-              {showCard && cardDetails && cardDetails.status === 'success' ? (
-                <>
-                  <ScratchCard
-                    width={300}
-                    height={400}
-                    image={backgroundImageSrc}
-                    finishPercent={30}
-                    fadeOutOnComplete
-                    onComplete={(e) => addDigiDollasHandler(e)}
-                  >
-                    <div
-                      className='scratch-card-bg'
-                      style={{
-                        width: '100%',
-                        height: '100%',
-                      }}
-                    >
-                      <>
-                        <Row className='no-gutters heading'>Congrats!</Row>
-                        <Row className='no-gutters coinDigiDollas'>
-                          <Image
-                            className='scratch-coin'
-                            fluid
-                            src={cardDetails.product.image}
-                            alt={''}
-                            rounded
-                          ></Image>
-                        </Row>
-                        <Row className='no-gutters message'>{`Yay! You've won ${cardDetails.product.name}`}</Row>
-                        <Row className='note'>
-                          <p>{`This will be credited to your DigiDollas`}</p>
-                        </Row>
-                      </>
-                    </div>
-                  </ScratchCard>
-                  <div className='text-center'>
-                    <Button
-                      className='mt-4 btn btn-primary digicel-button'
-                      onClick={handleCardClose}
-                    >
-                      New Card
-                    </Button>
-                  </div>
-                </>
-              ) : cardDetails && cardDetails.status === 'fail' ? (
-                <>
-                  <div
-                    className='ScratchCard__Container'
-                    // style={{ width: '300px', height: '330px' }}
-                  >
-                    <img
-                      className='img-fluid mx-auto'
-                      src={backgroundImageAlreadyScratched}
-                      alt=''
-                    ></img>
-                  </div>
-                  <h2 className='my-3'>
-                    Sorry! <br /> Come back later
-                  </h2>
-                  <h2>
-                    <Countdown
-                      className='digicel-gradient-text'
-                      date={dayjs() + dayjs().endOf('day').diff(dayjs())}
-                    />
-                  </h2>
-                </>
-              ) : (
-                <>
-                  <Form onSubmit={handleCardShow}>
-                    <Form.Group className='mb-3' controlId='mobile'>
-                      <Form.Label>Mobile Number</Form.Label>
-                      <Form.Control
-                        type='mobile'
-                        placeholder='Enter Mobile'
-                        value={mobile}
-                        onChange={(e) => setMobile(e.target.value)}
-                        required
-                      />
-                      <Form.Text className='text-muted'>
-                        Format: 1234567
-                      </Form.Text>
-                    </Form.Group>
+            <>
+              <Form onSubmit={handleCardShow}>
+                <Form.Group className='mb-3' controlId='mobile'>
+                  <Form.Label>Mobile Number</Form.Label>
+                  <Form.Control
+                    type='mobile'
+                    placeholder='Enter Mobile'
+                    value={mobile}
+                    onChange={(e) => setMobile(e.target.value)}
+                    required
+                  />
+                  <Form.Text className='text-muted'>Format: 1234567</Form.Text>
+                </Form.Group>
 
-                    <Form.Group controlId='image'>
-                      <Form.Label>Image</Form.Label>
-                      <Form.Control
-                        className='mb-3'
-                        type='file'
-                        onChange={uploadFileHandler}
-                        label='Choose File'
-                        required
-                      />
-                      {uploading && <Loader />}
-                      <Form.Control
-                        className='mb-3'
-                        type='text'
-                        placeholder='Image URL'
-                        value={image}
-                        onChange={(e) => setImage(e.target.value)}
-                        disabled
-                      ></Form.Control>
-                    </Form.Group>
-                    <Button
-                      variant='primary'
-                      type='submit'
-                      className='btn btn-primary digicel-button'
-                    >
-                      Show Card
-                    </Button>
-                  </Form>
-                </>
-              )}
-            </div>
+                <Form.Group controlId='image'>
+                  <Form.Label>Image</Form.Label>
+                  <Form.Control
+                    className='mb-3'
+                    type='file'
+                    onChange={uploadFileHandler}
+                    label='Choose File'
+                    required
+                  />
+                  {uploading && <Loader />}
+                  <Form.Control
+                    className='mb-3'
+                    type='text'
+                    placeholder='Image URL'
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)}
+                    disabled
+                  ></Form.Control>
+                </Form.Group>
+                <Button
+                  variant='primary'
+                  type='submit'
+                  className='btn btn-primary digicel-button'
+                >
+                  Show Card
+                </Button>
+              </Form>
+            </>
           </Card.Body>
         </Card>
       </Container>
