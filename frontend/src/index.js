e1922e1a485a7aa7f292fefe857c19e491ff7007
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2022-07-11 11:01:13 +0530

    2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import './assets/scss/style.scss'
 import './index.css'
 
 import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 import reportWebVitals from './reportWebVitals'
 
@@ -21,3 +22,15 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  // dev code
+  serviceWorker.unregister()
+} else {
+  // production code
+  serviceWorker.register()
+}
